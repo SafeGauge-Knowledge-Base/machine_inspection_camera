0fb5ed7f0740967e1a0e8ea3d4d8adfe7e7693b0
scott-olero <75195501+scott-olero@users.noreply.github.com>
2024-08-23 11:43:17 +1000

    original example.

--- android/app/src/main/kotlin/com/example/machine_inspection_camera/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/machine_inspection_camera/MainActivity.kt
@@ -1,45 +1,5 @@
 package com.example.machine_inspection_camera
 
-import android.os.Bundle
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import com.arashivision.sdkcamera.InstaCameraSDK
-import com.arashivision.sdkcamera.FlowCameraManager
 
-class MainActivity : FlutterActivity() {
-
-    private val CHANNEL = "flow_sdk"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        InstaCameraSDK.initFlow(this)
-    }
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "initializeFlowSdk" -> {
-                    InstaCameraSDK.initFlow(applicationContext)
-                    result.success(null)
-                }
-                "startBleScan" -> {
-                    val duration: Int? = call.argument("duration")
-                    duration?.let {
-                        FlowCameraManager.getInstance().startBleScan(it)
-                        result.success(null)
-                    } ?: run {
-                        result.error("INVALID_ARGUMENT", "Duration is required", null)
-                    }
-                }
-                else -> {
-                    result.notImplemented()
-                }
-            }
-        }
-    }
-}
+class MainActivity: FlutterActivity()
